af8dac83444d32a4edbb684fcbb2c6e6201f02aa
Alok Das <contact.alokdas@gmail.com>
2026-01-07 16:27:16 +0530

    changing CSRF_TRUSTED_ORIGINS and allowed origins

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ofz5)9bx7*-o68tyflkd@zbp%q3v7#hocbj9q(r%)*5l6u4yn!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','139.59.88.65','localhost','64.227.161.51']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','139.59.75.243']
 
 
 # Application definition
@@ -99,12 +99,12 @@ DATABASES = {
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://64.227.161.51",
+    "http://139.59.75.243",
     "http://localhost:3000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://64.227.161.51",
+    "http://139.59.75.243",
 ]
 
 # Password validation
